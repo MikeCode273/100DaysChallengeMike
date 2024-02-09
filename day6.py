30260836391b900edf3cbe820d13af4459e3fdd3
MikeCoder <mkfenteng@st.ug.edu.gh>
2024-02-09 14:41:47 +0000

    day 7 commit

---
+++ day6.py
@@ -0,0 +1,19 @@
+#How to create functions
+
+# def my_function():
+#   print("Hello world")
+#   print("Michael")
+
+# my_function()
+# my_function()
+
+#while loop
+
+while True:
+  print("YESSSS!!")
+  print("YESSSS!!")
+  print("YESSSS!!")
+  print("YESSSS!!")
+  print("YESSSS!!")
+  print("YESSSS!!")
+  print("YESSSS!!")
